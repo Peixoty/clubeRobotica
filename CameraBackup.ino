c3b5b3925630f78c327d37117e3446e6f48da702
Luiz Henrique <110421695+Peixoty@users.noreply.github.com>
2024-10-09 09:08:45 -0300

    Add files via upload

--- CameraBackup.ino
+++ CameraBackup.ino
@@ -34,7 +34,7 @@ BluetoothSerial SerialBT;
 #define IN3 12
 #define IN4 13
 
-#define SetPoint 40 // vel base do motor
+#define SetPoint 32 // vel base do motor
 
 void setup() {
 
@@ -76,24 +76,24 @@ void setup() {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
-
+  
   if(!SerialBT.begin("ESP32CAM")) {
     Serial.println("Erro ao iniciar o Bluetooth");
   } else {
     Serial.println("Bluetooth iniciado com sucesso. Agora você pode parear com o ESP32CAM.");
   }
-
+  
   //inicialização motor
   pinMode(IN1, OUTPUT); 
   pinMode(IN2, OUTPUT); 
   pinMode(IN3, OUTPUT); 
   pinMode(IN4, OUTPUT);
 
-  digitalWrite(IN1, LOW);
-  digitalWrite(IN2, LOW);
+  analogWrite(IN1, 0);
+  analogWrite(IN2, 0);
 
-  digitalWrite(IN4, LOW);
-  digitalWrite(IN3, LOW);
+  analogWrite(IN4, 0);
+  analogWrite(IN3, 0);
 
 }
 
@@ -107,7 +107,7 @@ camera_fb_t *fb = NULL;
 #define Roboj 47.5 // centro do eixo em coordenadas
 #define difRodas (6/0.1) // distancia das rodas em pixels
 
-#define difMotor 0.63 // balanceamento de motor
+#define difMotor 0.8 // balanceamento de motor
 
 void printCamera(){ // printa camera em uma matriz
   for(int i=0; i<fb->width; i++){
@@ -119,22 +119,6 @@ void printCamera(){ // printa camera em uma matriz
   Serial.println();
   delay(2000);
 }
-void printCameraBT(){
-  for(int i=0; i<fb->width; i++){
-    for(int j=0; j<fb->width; j++){
-      if (SerialBT.available()) {
-        SerialBT.print(pixel(i,j));
-      }
-    }
-    if (SerialBT.available()) {
-        SerialBT.println();
-      }
-  }
-  if (SerialBT.available()) {
-    SerialBT.println();
-  }
-  //delay(2000);
-}
 
 bool pixel(int i, int j) {  // função que verifica o valor do pixel
     return (fb->buf[i * 96 + j]>Carlos)?true:false; // converte o valor de 0 a 255 em booleano sendo os pontos brancos=1
@@ -145,19 +129,17 @@ class Desafios{
   bool cruz=false;
 
   public:
-  void checarDesafio(){ // Descobre qual o desafio a ser cumprido
-    if(qlido==1){ // Quadrado lado direito
-    // Não deixa contar o mesmo quadrado
-      for(int i=fb->width-1; i>=fb->width-3; i--){ 
-        for(int j=fb->width-1; j>=fb->width-2; j--){ 
+  void checarDesafio(){
+    if(qlido==1){
+      for(int i=fb->width-1; i>=fb->width-3; i--){
+        for(int j=fb->width-1; j>=fb->width-2; j--){
           if(pixel(i,j)){
             qlido=0;
           }
         }
       }
     }
-    else if(qlido==-1){ // Quadrado lado esquerdo
-    // Não deixa contar o mesmo quadrado
+    else if(qlido==-1){
       for(int i=fb->width-1; i>=fb->width-3; i--){
         for(int j=0; j<=1; j++){
           if(pixel(i,j)){
@@ -167,49 +149,59 @@ class Desafios{
       }
     }
     else{ 
-      if(quad>=0){ // Se não tem quadrado ou já apareceu um quadrado à direita, tenta contar outro quadrado
+      if(quad>=0){
         countQuadrado(true); // true lado da direita // false lado da esquerda
       }
-      if(quad<=0){ // Se não tem quadrado ou já apareceu um quadrado à esquerda, tenta contar outro quadrado
+      if(quad<=0){
         cruz=false;
         countQuadrado(false); // se for direita quad é positivo se for esquerda quad é negativo
       }
     }
+    SerialBT.print("quad = ");
+    SerialBT.println(quad);
+    SerialBT.print("cruz = ");
+    SerialBT.println(cruz);
+    SerialBT.print(" ");
 
-    if(cruz){ // Quando ler um cruzamento, entra aqui para resolver desafios
+    if(cruz){
       esp_camera_fb_return(fb);
-      if(quad==0){  // Desafio da ré em que a cruz aparece antes dos quadrados
+      if(quad==0){
+        SerialBT.print("cruz?");
         fb = esp_camera_fb_get();
-        cruz = confirmCruz(true); // Retorna se realmente tem uma cruz
+        cruz = confirmCruz(true);
         esp_camera_fb_return(fb);
-        if(cruz){
+        /*if(cruz){
+          SerialBT.print("re?");
           idRe();
-        }
+        }*/
       }
-      else if(abs(quad)==1){  // Se há algum quadrado lido
-        jmyself(quad/abs(quad));  // Vira pro lado que esse quadrado está
+      else if(abs(quad)==1){
+        SerialBT.print("jmyself");
+        jmyself(quad/abs(quad));
       }
-      else if(abs(quad)>1){ // Entra no desafio da rotatória
+      /*else if(abs(quad)>1){
+        SerialBT.print("rotatoria");
         rot();
-      }
+      }*/
       fb = esp_camera_fb_get();
-      qlido=0;  // Reseta o lado do quadrado lido
-      quad=0;   // Reseta o numero de quadrados
-      cruz=false; // Reseta o numero de cruzes
-    } else if (quad == 0 && identificaInversaoCores()){ // Começa o desafio da faixa de pedestre se não tiver nenhum quadrado e identificar a faixa de pedestre
+      qlido=0;
+      quad=0;
+      cruz=false;
+    } /*else if (quad == 0 && identificaInversaoCores()){ // Começa o desafio da faixa de pedestre se não tiver nenhum quadrado e identificar a faixa de pedestre
+      SerialBT.print("faixa");
       desafioFaixaPedestre();
-    }
+    }*/
   }
   private:
   void countQuadrado(int sentido){// true lado da direita // false lado da esquerda
     for(int i=fb->width-1; i>=0; i--){
-      int count=0; // Contador de uns
+      int count=0;
       while(pixel(i,(fb->width-1)*sentido) || pixel(i,(fb->width-3)*sentido+1)){
         count++;
-        if(i<=2 || i>=fb->width-3){ // Se o quadrado estiver nas bordas, reseta o count
+        if(i<=2 || i>=fb->width-3){
           count=0;
           if(i>=fb->width-3){
-            while(pixel(i,(fb->width-1)*sentido) || pixel(i,(fb->width-3)*sentido+1)){ // Enquanto tiver lendo uns dos quadrados, diminui i
+            while(pixel(i,(fb->width-1)*sentido) || pixel(i,(fb->width-3)*sentido+1)){
               i--;
             }
           }
@@ -220,12 +212,12 @@ class Desafios{
         }
         i--;
       }
-      if(count!=0){ // Se tiver lido algum pixel branco
-        if(count>25){ // E o numero de pixels branco for maior que 25, é um quadrado
-          quad+=2*sentido-1;  // += 1 ou -1 dependendo do sentido
-          qlido=2*sentido-1;  // = 1 ou -1 dependendo do sentido
+      if(count!=0){
+        if(count>25){
+          quad+=2*sentido-1;
+          qlido=2*sentido-1;
         }
-        else if(count>7){ // E o número de pixels for menor que 25, mas maior do que 7, é uma cruz
+        else if(count>7){
           cruz=true;
         }
       }
@@ -235,7 +227,7 @@ class Desafios{
   bool confirmCruz(int sentido){// true lado da direita // false lado da esquerda
     for(int i=fb->width-1; i>=0; i--){
       int count=0;
-      while(pixel(i,(fb->width-1)*sentido) || pixel(i,(fb->width-3)*sentido+1)){
+      while((pixel(i,(fb->width-1)*sentido) || pixel(i,(fb->width-3)*sentido+1)) && i!=0){
         count++;
         i--;
       }
@@ -245,44 +237,70 @@ class Desafios{
     }
     return false;
   }
+  /*
+  void jmyself(int sentido){ // negativo direita // positivo esquerda
+    analogWrite(IN2, (25-(sentido*25))*difMotor);
+    analogWrite(IN3, 25+(sentido*25));
+    delay(1000);
+    analogWrite(IN2, 60);
+    analogWrite(IN3, 60);
+    delay(200);
+  }
+  */
 
   void jmyself(int sentido){ // negativo direita // positivo esquerda
     float linha;
     bool direcaoGiro = (sentido == 1)? true:false;
-    esp_camera_fb_return(fb);
     // Anda por 100ms confere se a linha está alinhada. Repete até ver uma linha com media na coluna 48
+    delay(150);
+    analogWrite(IN2, 50*(!direcaoGiro));
+    analogWrite(IN3, 50*direcaoGiro);
+    delay(500);
+
     do{  
-      analogWrite(IN2, (25-(sentido*25))*difMotor);
-      analogWrite(IN3, 25+(sentido*25));
-      delay(100);
+      analogWrite(IN2, 45*(!direcaoGiro));
+      analogWrite(IN3, 45*direcaoGiro);
+
       fb = esp_camera_fb_get();
-      linha = mediaLinha(30,false);
+      linha = mediaLinha(20,false);
+      SerialBT.println(linha);
       esp_camera_fb_return(fb);
-    } while((linha < (40 - direcaoGiro*5)) || (linha > (56 + !direcaoGiro*5))); // Enquanto a linha não estiver num range de 16 do centro, continua virando
-    fb = esp_camera_fb_get();
+
+    } while((linha > (56 + direcaoGiro*5)) || (linha < (40 - !direcaoGiro*5))); // Enquanto a linha não estiver num range de 16 do centro, continua virando
+
+    analogWrite(IN2, 0);
+    analogWrite(IN3, 0);
+
+    delay(500);
+
+    parteMotor(45);
+
   }
 
   void rot(){
-    bool s; // Lado para qual a cruz vai estar
+    bool s;
     if(quad>0){
       s=true;
     }
     else{
       s=false;
     }
-    jmyself(quad/abs(quad));  // Vira os primeiros 90°
-    while(abs(quad)>1){ // Faz seguir linha até chegar na saída certa
+    jmyself(quad/abs(quad));
+    //fb = esp_camera_fb_get();
+    //esp_camera_fb_return(fb);
+    while(abs(quad)>1){
       fb = esp_camera_fb_get();
       float Linhaj=mediaLinha(alturaLeitura, false);
       float Linhai=abs(47.5-Linhaj)+alturaLeitura; // transformação da media das colunas na media de linhas
       float r=raio(Linhai, Linhaj); // manda as coordenadas lidas para o raio
       ajusteMotor(r);
-      if(confirmCruz(s)){  // Se achar um cruzamento do lado certo, diminui o número de quadrados até ele chegar em 1
+      cruz=confirmCruz(s);
+      if(cruz){
         quad-=(quad/abs(quad));
       }
       esp_camera_fb_return(fb);
     }
-    jmyself(quad);  // Vira na saída certa
+    jmyself(quad);
   }
 
   void idRe(){
@@ -304,6 +322,7 @@ class Desafios{
       esp_camera_fb_return(fb);
       if(quad!=0){
         marchaRe();
+        break;
       }
     }
   }
@@ -322,8 +341,9 @@ class Desafios{
       }
       esp_camera_fb_return(fb);
     }
-    digitalWrite(IN2, LOW);
-    digitalWrite(IN3, LOW);
+    analogWrite(IN2, 0);
+    analogWrite(IN3, 0);
+    delay(1000);
     analogWrite(IN1, SetPoint*difMotor);
     analogWrite(IN4, SetPoint);
     count=0;
@@ -337,121 +357,124 @@ class Desafios{
     }
     digitalWrite(IN1, LOW);
     digitalWrite(IN4, LOW);
+    delay(1000);
     jmyself(-quad);
   }
 
   bool identificaInversaoCores() {
-  int j;
-  int countPreto = 0, countBranco = 0;
+    int j;
+    int countPreto = 0, countBranco = 0;
 
-  // Faz a contagem dos pixels na linha 10
-  for (j = 0; j < fb->width; j++) {
-    if (pixel(10, j)) {
-      countBranco++;
+    // Faz a contagem dos pixels na linha 10
+    for (j = 0; j < fb->width; j++) {
+      if (pixel(10, j)) {
+        countBranco++;
+      } else {
+        countPreto++;
+      }
+    }
+
+    //if (countPreto >= countBranco) {
+    /*SerialBT.println("Branco = ");
+    SerialBT.println(countBranco/(fb->width));*/
+    if (countBranco/(fb->width) <= 0.78) {
+      return false;  // A linha é branca
     } else {
-      countPreto++;
+      return true;  // Linha é preta
     }
   }
-
-  if (countPreto >= countBranco) {
-    return false;  // A linha é branca
-  } else if (countBranco > countPreto) {
-    return true;  // Linha é preta
-  }
-}
 
   int identificaFaixaPedestre() {
-  int z = 0, j = 0, count = 0;
-  bool aux = true;
+    int z = 0, j = 0, count = 0;
+    bool aux = true;
 
-  while (aux) {
+    while (aux) {
 
-    if (pixel(40, j) && pixel(40, j + 1) && pixel(40, j + 2)) {  // Achou uma faixa se encontrar 3 pixels brancos consecutivos 00001111111111100000000001111111
-      count++;
-      for (z = j; z < (fb->width) - 2; z++) {                             // Busca por um pixel preto
-        if (!pixel(40, z) && !pixel(40, z + 1) && !pixel(40, z + 2)) {  // Achou um entre faixa (preto)
-          j = z;                                                        // Atualiza de onde o próximo J vai começar
-          break;
+      if (pixel(40, j) && pixel(40, j + 1) && pixel(40, j + 2)) {  // Achou uma faixa se encontrar 3 pixels brancos consecutivos 00001111111111100000000001111111
+        count++;
+        for (z = j; z < (fb->width) - 2; z++) {                             // Busca por um pixel preto
+          if (!pixel(40, z) && !pixel(40, z + 1) && !pixel(40, z + 2)) {  // Achou um entre faixa (preto)
+            j = z;                                                        // Atualiza de onde o próximo J vai começar
+            break;
+          }
+          if (z == (fb->width) - 3) {  // Se z chegar no limite, quebra o while
+            aux = false;
+          }
         }
-        if (z == (fb->width) - 3) {  // Se z chegar no limite, quebra o while
-          aux = false;
-        }
+      }
+
+      j++;
+
+      if(j == fb->width - 3){
+        aux = false;
+      }
+
+      if (count > 1) {
+        //SerialBT.println("Faixa de Pedestre");
+        return 1;  // 1 representa a faixa de pedestre
       }
     }
 
-    j++;
-
-    if(j == fb->width - 3){
-      aux = false;
-    }
-
-    if (count > 1) {
-      SerialBT.println("Faixa de Pedestre");
-      return 1;  // 1 representa a faixa de pedestre
+    if (count == 1) {
+      //SerialBT.println("Volta a seguir a linha");
+      return 2;  // 2 representa a linha que ele deve voltar a seguir
+    } else if (count == 0) {
+      //SerialBT.println("Tudo preto");
+      return 0;  // 0 representa que não há linha a ser seguida
     }
   }
-
-  if (count == 1) {
-    SerialBT.println("Volta a seguir a linha");
-    return 2;  // 2 representa a linha que ele deve voltar a seguir
-  } else if (count == 0) {
-    SerialBT.println("Tudo preto");
-    return 0;  // 0 representa que não há linha a ser seguida
-  }
-}
 
   void desafioFaixaPedestre() {
-  bool semLinha = false;  // Inicialmente há linha a ser seguida
-  int j, count = 0, estadoLinha;
-  float Linhaj, Linhai, r;
-
-  esp_camera_fb_return(fb);
-
-  while (!semLinha) {  // Enquanto tiver linha a ser seguida
-    fb = esp_camera_fb_get();
-
-    // Segue a linha
-    Linhaj = mediaLinha(10, true);     // Coloca o vértice do V na linha 10
-    Linhai = abs(47.5 - Linhaj) + 10;  // transformação da media das colunas na media de linhas
-    r = raio(Linhai, Linhaj);          // manda as coordenadas lidas para o raio
-
-    ajusteMotor(r);
-
-    for (j = 0; j < fb->width; j++) {
-      if (!pixel(30, j)) {  //Se a 30 linha tem muito pixel preto
-        count++;
-      }
-    }
-
-    if (count > 90) {  // Se tiver mais de 90 pixeis pretos, ele chegou no fim da linha branca
-      semLinha = true;  // Não Há linha
-
-    } else {
-      count = 0;
-    }
+    bool semLinha = false;  // Inicialmente há linha a ser seguida
+    int j, count = 0, estadoLinha;
+    float Linhaj, Linhai, r;
 
     esp_camera_fb_return(fb);
-  }
 
-  analogWrite(IN2, 0);
-  analogWrite(IN3, 0);
-  delay(5000);
+    while (!semLinha) {  // Enquanto tiver linha a ser seguida
+      fb = esp_camera_fb_get();
 
-  while (semLinha) {  // Enquanto ele não estiver enxergando a linha
-    fb = esp_camera_fb_get();
-    estadoLinha = identificaFaixaPedestre();
+      // Segue a linha
+      Linhaj = mediaLinha(10, true);     // Coloca o vértice do V na linha 10
+      Linhai = abs(47.5 - Linhaj) + 10;  // transformação da media das colunas na media de linhas
+      r = raio(Linhai, Linhaj);          // manda as coordenadas lidas para o raio
 
-    if (estadoLinha == 0 || estadoLinha == 1) {  // Se tiver faixa de pedestre ou tudo preto, continua reto
-      analogWrite(IN2, 120);
-      analogWrite(IN3, 120);
+      ajusteMotor(r);
+
+      for (j = 0; j < fb->width; j++) {
+        if (!pixel(30, j)) {  //Se a 30 linha tem muito pixel preto
+          count++;
+        }
+      }
+
+      if (count > 90) {  // Se tiver mais de 90 pixeis pretos, ele chegou no fim da linha branca
+        semLinha = true;  // Não Há linha
+
+      } else {
+        count = 0;
+      }
+
       esp_camera_fb_return(fb);
-    } else if (estadoLinha == 2) {  // Se achar a linha de volta, volta a seguir linha
-      esp_camera_fb_return(fb);
-      return;
+    }
+
+    analogWrite(IN2, 0);
+    analogWrite(IN3, 0);
+    delay(5000);
+
+    while (semLinha) {  // Enquanto ele não estiver enxergando a linha
+      fb = esp_camera_fb_get();
+      estadoLinha = identificaFaixaPedestre();
+
+      if (estadoLinha == 0 || estadoLinha == 1) {  // Se tiver faixa de pedestre ou tudo preto, continua reto
+        analogWrite(IN2, 120);
+        analogWrite(IN3, 120);
+        esp_camera_fb_return(fb);
+      } else if (estadoLinha == 2) {  // Se achar a linha de volta, volta a seguir linha
+        esp_camera_fb_return(fb);
+        return;
+      }
     }
   }
-}
-
 };
 
 Desafios des;
@@ -459,16 +482,15 @@ void loop() {
   
   fb = esp_camera_fb_get(); // preenche o vetor com a leitura atual da camera
   des.checarDesafio();
-  //ajusteMotor(erroFx1, fatorErroAngulo(erroFx1, erroFx2)); // ajusta o motor com base no erro de centralização e angulação da linha
+  seguirLinha();
+  esp_camera_fb_return(fb); // esvazia o vetor preenchido pela leitura da camera
+}
+
+void seguirLinha(){
   float Linhaj=mediaLinha(alturaLeitura, false);
   float Linhai=abs(47.5-Linhaj)+alturaLeitura; // transformação da media das colunas na media de linhas
   float r=raio(Linhai, Linhaj); // manda as coordenadas lidas para o raio
   ajusteMotor(r);
-  //printCameraBT();
-  //printCamera();
-  //analogWrite(IN2, SetPoint*0.63);
-  //analogWrite(IN3, SetPoint);
-  esp_camera_fb_return(fb); // esvazia o vetor preenchido pela leitura da camera
 }
 
 void ajusteMotor(float r){
@@ -479,6 +501,7 @@ void ajusteMotor(float r){
   analogWrite(IN3, SetPoint*abs((r-difRodas*2)/r));
   }
 }
+
 float mediaLinha(int a, bool corLinha){// ponto medio da linha
   int som=0; // somatorio de colunas
   int peso=0; // numero de uns
@@ -490,6 +513,7 @@ float mediaLinha(int a, bool corLinha){// ponto medio da linha
   }
   return (peso == 0)? 0:som/peso; // media das colunas lidas
 }
+
 float raio(float i, float j){
   float di, dj, hip, cos, r;
   di=Roboi-i; // distancia entre as coordenadas do eixo do robo e da media da linha
@@ -502,7 +526,14 @@ float raio(float i, float j){
   return (hip/2)/cos; // raio da circunferencia
 }
 
-
+void parteMotor(int sp){
+  int i;
+  for(i = 1; i <= sp; i++){
+    analogWrite(IN2, i);
+    analogWrite(IN3, i);
+    delay(50);
+  }
+}
 
 
 
